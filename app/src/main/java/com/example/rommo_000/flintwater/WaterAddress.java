837cbc17ff1f1574fcc4691acec5727d021443d4
ryankroll <rmkroll@hotmail.com>
2016-03-27 16:22:10 -0400

    Now placing pins at all the locations in firebase database.

---
+++ app/src/main/java/com/example/rommo_000/flintwater/WaterAddress.java
@@ -0,0 +1,33 @@
+package com.example.rommo_000.flintwater;
+
+/**
+ * Created by rkroll on 3/15/2016.
+ */
+// Class to hold address from firebase.
+public class WaterAddress {
+        private double latitude;
+        private double longitude;
+        private String location;
+        private String city;
+        private String state;
+        private String address;
+        private double zipCode;
+
+        public WaterAddress(){
+            // Defualt Constructor
+        }
+
+        public double getLongitude() {return longitude;}
+
+        public double getLatitude() {return latitude;}
+
+        public String getLocation() {return location;}
+
+        public String getCity() {return city;}
+
+        public String getState() {return state;}
+
+        public double getZipCode() {return zipCode;}
+
+        public String getAddress() {return address;}
+    }
--- app/src/main/java/com/example/rommo_000/flintwater/show_water.java
+++ app/src/main/java/com/example/rommo_000/flintwater/show_water.java
@@ -1,33 +1,38 @@
 package com.example.rommo_000.flintwater;
 
-import android.content.AsyncTaskLoader;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.introspect.VisibilityChecker;
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
 import com.firebase.client.ValueEventListener;
 
-import com.firebase.geofire.GeoFire;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class show_water extends FragmentActivity implements OnMapReadyCallback {
+public class show_water extends FragmentActivity implements OnMapReadyCallback,
+        GoogleMap.OnInfoWindowClickListener {
 
     private GoogleMap mMap;
 
     // get a reference to firebase database
-    Firebase ref = new Firebase("https://incandescent-fire-5642.firebaseio" +
-            ".com/");
+    Firebase ref = new Firebase("https://incandescent-fire-5642.firebaseio.com/WaterAddress");
 
-
+    //Query queryRef = ref.orderByChild("Name");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,8 +41,9 @@ public class show_water extends FragmentActivity implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        Firebase.setAndroidContext(this);
+
     }
+
 
 
     /**
@@ -54,73 +60,39 @@ public class show_water extends FragmentActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-       // LatLng sydney = new LatLng(-34, 151);
-       // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-       // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        // LatLng sydney = new LatLng(-34, 151);
+        // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
         LatLng flint = new LatLng(43.0100, -83.6900);
         mMap.addMarker(new MarkerOptions().position(flint).title(getString(R.string.flint_marker)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(flint));
 
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(flint, 13));
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(flint, 16));
+        mMap.setOnInfoWindowClickListener(this);
+        //Place markers on map
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot snapshot) {
 
-        // Call PlaceMarkers on a different thread to place markers on the map.
-        new PlaceMarkers().execute(ref);
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    WaterAddress address = postSnapshot.getValue(WaterAddress.class);
+                    mMap.addMarker((new MarkerOptions().position((new LatLng(address.getLatitude
+                            (), address.getLongitude()))).title(address.getLocation())));
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+                Log.d("Failure: ", firebaseError.getMessage());
+            }
+        });
 
     }
-    private class PlaceMarkers extends AsyncTask<Firebase, Void, Void> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected Void doInBackground(Firebase... params) {
-            //Check for new values added to database
-            ref.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(DataSnapshot dataSnapshot) {
-                    for (DataSnapshot postSnapShot : dataSnapshot.getChildren()) {
-                        WaterAddress address = postSnapShot.getValue(WaterAddress.class);
-                        mMap.addMarker(new MarkerOptions()
-                                .position(new LatLng(address.getLatitude(), address.getLongitude())));
-                    }
-                }
-
-                @Override
-                public void onCancelled(FirebaseError firebaseError) {
-                }
-            });
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-        }
-    }
-// Class to hold address from firebase.
-    public class WaterAddress {
-        private String city;
-        private String close;
-        private String name;
-        private String open;
-        private String state;
-        private String street;
-        private int zipCode;
-        private double latitude;
-        private double longitude;
-
-        public WaterAddress(){ }
-
-        public double getLongitude() {
-            return longitude;
-        }
-
-        public double getLatitude() {
-            return latitude;
-        }
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        Log.d("Success: ", "Info windows was clicked");
     }
 
 }
